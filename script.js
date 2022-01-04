0e3a6a43d857bf288a992ff8f7e7f0127d4e80f1
soumya123raj <78701991+soumya123raj@users.noreply.github.com>
2022-01-04 21:43:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<!-- Core -->
+<script src="/assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Argon JS -->
+<script src="/assets/js/argon.min.js"></script>
